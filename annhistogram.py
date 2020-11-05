249d338617feefd9bf4d9a4ae1a53d0e50a9d963
Kosachev Daniil <kos-dan@mail.ru>
2020-11-05 14:22:39 +0300

    Draft of 'main'

--- annhistogram.py
+++ annhistogram.py
@@ -1,5 +1,12 @@
+import sys
+from matplotlib import pyplot as plt
+
+
 def main():
-    pass
+    filename = _parse_args(sys.argv)
+    codes = _read_annotations(filename)
+    _plot_histogram(codes)
+    plt.show()
 
 
 if __name__ == "__main__":
