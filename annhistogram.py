97bb15396bd224c0148358d7253e4e206a6ddb1f
Kosachev Daniil <kos-dan@mail.ru>
2020-11-05 17:22:19 +0300

    Added _plot_items_hist

--- annhistogram.py
+++ annhistogram.py
@@ -1,7 +1,9 @@
 import sys
 import json
+from collections import Counter
 from matplotlib import pyplot as plt
 import numpy as np
+import pandas
 from ecganncmp import Text
 
 
@@ -42,6 +44,13 @@ def _plot_histogram(codes):
     plt.bar(bins[1][:-1] + width, heights[1], width=width, label="test")
     plt.legend()
     plt.title("Records count: %d" % len(codes))
+
+
+def _plot_items_hist(items, axes):
+    counts = Counter(items)
+    dataframe = pandas.DataFrame.from_dict(counts, orient="index")
+    dataframe.sort_index(inplace=True, key=_code_to_numeric)
+    dataframe.plot(ax=axes, kind="bar", legend=False)
 
 
 if __name__ == "__main__":
