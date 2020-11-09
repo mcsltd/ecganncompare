0f0361066de511826128431792c92c82cad854e9
Kosachev Daniil <kos-dan@mail.ru>
2020-11-09 12:05:47 +0300

    Implement _plot_histogram

--- cmphistogram.py
+++ cmphistogram.py
@@ -2,6 +2,7 @@ import sys
 import json
 from collections import namedtuple
 from matplotlib import pyplot as plt
+import pandas
 from ecganncmp import Text
 
 ComparingInfo = namedtuple("ComparingInfo",
@@ -34,8 +35,25 @@ def _read_annotations(filename):
     return codes, info
 
 
-def _plot_histograms(codes, info):
-    raise NotImplementedError()
+def _plot_histogram(codes, info):
+    title = "Annotations comparing"
+    counts = {}
+    for rec_pairs in codes:
+        for pair in rec_pairs:
+            code = _get_code(pair)
+            code_counts = counts.setdefault(code, [0, 0])
+            if pair[0] == pair[1]:
+                code_counts[0] += 1
+            else:
+                code_counts[1] += 1
+    df = pandas.DataFrame.from_dict(counts, orient="index")\
+                         .sort_index()\
+                         .rename(columns={0: "Matches", 1: "Misses"})
+    df.plot(ax=plt.gca(), kind="bar", legend=True)
+
+
+def _get_code(pair):
+    return pair[0] if pair[0] is not None else pair[1]
 
 
 if __name__ == "__main__":
