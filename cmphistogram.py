aed68dae31a8d01641b67bb07e5bc9bfd9c7b9f1
Kosachev Daniil <kos-dan@mail.ru>
2020-11-16 17:13:45 +0300

    Fixed import

--- cmphistogram.py
+++ cmphistogram.py
@@ -1,7 +1,7 @@
 import os
 import sys
 import json
-from collections import namedtuple, defaultdict
+from collections import namedtuple, defaultdict, Counter
 from matplotlib import pyplot as plt
 import pandas
 
