568e13de488b9cabe46645af2af49c65c8034f9c
Kosachev Daniil <kos-dan@mail.ru>
2020-11-17 12:17:04 +0300

    Refactoring

--- anndistribution.py
+++ anndistribution.py
@@ -155,11 +155,11 @@ def _print_removed_items(items, fieldname):
 
 
 def _print_groups_info(data_groups):
-    annotators = list(data_groups.keys())
-    thesaurus = data_groups[annotators[0]][0][Text.CONCLUSION_THESAURUS]
+    temp_annotator = next(iter(data_groups))
+    thesaurus = data_groups[temp_annotator][0][Text.CONCLUSION_THESAURUS]
     print("Thesaurus: %s" % thesaurus)
     print("Annotation groups:\n")
-    for annotator in annotators:
+    for annotator in data_groups:
         print("Annotator: " + annotator)
         data_list = data_groups[annotator]
         print("Records Count: %d" % len(data_list))
