267328f4c1a13a3bc8813628b4234ad16a809278
Kosachev Daniil <kos-dan@mail.ru>
2020-12-10 12:05:49 +0300

    Added json reading dunctions

---
+++ anngroupscmp.py
@@ -0,0 +1,22 @@
+import os
+import codecs
+from collections import OrderedDict
+import json
+
+
+def _read_json_folder(dirname):
+    all_paths = (os.path.join(dirname, x) for x in os.listdir(dirname))
+    all_files = [p for p in all_paths
+                 if os.path.isfile(p) and p.lower().endswith(".json")]
+    results = []
+    for fname in all_files:
+        try:
+            results.append(_read_json(fname))
+        except ValueError:
+            continue
+    return results
+
+
+def _read_json(filename):
+    with codecs.open(filename, "r", encoding="utf-8") as fin:
+        return json.load(fin, object_pairs_hook=OrderedDict)
