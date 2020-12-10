1fa705d1e2db84d2a31db7152b148acee0db40c7
Kosachev Daniil <kos-dan@mail.ru>
2020-12-10 12:52:29 +0300

    Added _read_data

--- anngroupscmp.py
+++ anngroupscmp.py
@@ -147,3 +147,18 @@ def _create_stats_dataframe(datatables, total_ann_count):
     dframe.index = annotators
     dframe.columns = annotators
     return dframe
+
+
+def _read_data(input_paths):
+    all_jsons = []
+    # TODO: check input_path is filename or dirs list
+    warning_fmt = (
+        "Warning! This program works only with data folders. File {0} will "
+        "be ignored."
+    )
+    for path in input_paths:
+        if os.path.isfile(path):
+            print(warning_fmt.format(path))
+        else:
+            all_jsons += _read_json_folder(path)
+    return all_jsons
