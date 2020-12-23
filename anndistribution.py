bbadd4be4c32278f32b473de57ac79cb326bc203
Kosachev Daniil <kos-dan@mail.ru>
2020-12-23 15:57:13 +0300

    Added _plot_common_histogram

--- anndistribution.py
+++ anndistribution.py
@@ -372,6 +372,16 @@ def _group_dataframes(dataframes, max_sum_group_size=10):
     return groups
 
 
+def _plot_common_histogram(dataframe, thesaurus):
+    dataframe = dataframe.sum(axis=1)
+    if thesaurus is not None:
+        sorted_index = (k for k in thesaurus.items if k in dataframe.index)
+        dataframe = dataframe.loc[sorted_index]
+        dataframe.index = [thesaurus.items[k] for k in dataframe.index]
+    plt.figure()
+    _plot_dataframe(dataframe)
+
+
 if __name__ == "__main__":
     try:
         main()
